aca91fa8dd762e22b5d11f1ff637543dcb85c26b
danmarksteiner <danielmarksteiner@feref.com>
2019-11-18 17:28:25 +0000

    Sets up testing app and first react redux tests

---
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<Root><CommentBox /></Root>);
+});
+
+afterEach(() => {
+    wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+        wrapped.update();
+    })
+    it('has a text area that a user can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    it('submits the form and empties the textarea', () => {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+});
